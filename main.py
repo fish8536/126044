dc80b9dd8928496314ac899be4a676b40e763b15
FISH-PC\fish <yjwang666@gmail.com>
2018-09-08 08:54:13 +0800

    fft complete

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
+from scipy.fftpack import fft,ifft
 
 def read_xls(file):
 	xls = pd.read_excel(file, index_col=None, header=None)
@@ -13,26 +14,36 @@ def predata(xls):
 	columns = xls[:-1][:].astype('float64')
 	return columns
 
-def predata_fft(xls):
-	fcolumns1 = np.fft.fft(xls[0][0:7499])
-	fcolumns2 = np.fft.fft(xls[1][0:7499])
-	fcolumns3 = np.fft.fft(xls[2][0:7499])
-	fcolumns4 = np.fft.fft(xls[3][0:7499])
-	yf=abs(fft(fcolumns1))
-	yf1=abs(fft(fcolumns1))/7500
+def predata_fft(col):
+	fcolumns = np.fft.fft(col)
+	yf=abs(fft(fcolumns))
+	yf1=abs(fft(fcolumns))/7500
 	yf2 = yf1[range(int(7500/2))]
+	return yf2
 
+def col_split(col):
+	columns_split = np.array_split(col, 5, axis=0)
+	return columns_split
 
-# tran_folder="traning_source/"
-# for f in listdir(tran_folder):
-# 	if isfile(join(tran_folder, f)):
-# 		read_xls(tran_folder+f)
+def col_max_average(col):
+	columns = []
+	for i in range(0,5):
+		columns.append(np.amax(col[i]))
+		columns.append(np.average(col[i]))
+	return columns
+# 40 file, 4 sensor, 5 fft max and avg
+result = np.zeros((40, 4, 10))
+tran_folder="traning_source/"
+tfi = 0
+for f in listdir(tran_folder):
+	if isfile(join(tran_folder, f)):
+		xls = read_xls(tran_folder+f)
+		pxls = predata(xls)
+		fcolumns = np.zeros((4,10))
+		for fc in range(4):
+			fcolumns[fc] = col_max_average(col_split(predata_fft(pxls[fc])))
+		result[tfi] = fcolumns
+		tfi=tfi+1
 
-
-
-
-xls = pd.read_excel("traning_source/20160419001_2016419_114348.xls", index_col=None, header=None)
-result = predata(xls)
-# for i in result:
 print(result)
-print(len(result))
+
