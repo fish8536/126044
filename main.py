ccdf3ac9d88643de8e72c1689dbb44386a2ee3dc
王渝鈞 <>
2018-09-08 13:38:05 +0800

    sync

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 from sklearn import metrics
 from os import listdir
 from os.path import isfile, join
